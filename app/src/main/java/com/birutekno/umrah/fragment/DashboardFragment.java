4c2a4cf46f41ebe576bf992e666ac66f1c6eedb7
Ismail Rohaga <azh.zhafir@gmail.com>
2018-06-08 09:41:51 +0700

    Dashboard view
    Adding temporary chart in dashboard fragment

--- app/src/main/java/com/birutekno/umrah/fragment/DashboardFragment.java
+++ app/src/main/java/com/birutekno/umrah/fragment/DashboardFragment.java
@@ -1,16 +1,31 @@
 package com.birutekno.umrah.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.birutekno.umrah.R;
+import com.birutekno.umrah.ui.chart.LineView;
 import com.birutekno.umrah.ui.fragment.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import butterknife.Bind;
 
 /**
  * Created by No Name on 7/29/2017.
  */
 
 public class DashboardFragment extends BaseFragment{
+
+    int randomint = 9;
+
+    @Bind(R.id.line_view)
+    LineView line;
+
+    @Bind(R.id.line_view_two)
+    LineView line_two;
 
     public static DashboardFragment newInstance() {
         DashboardFragment fragment = new DashboardFragment();
@@ -24,5 +39,71 @@ public class DashboardFragment extends BaseFragment{
 
     @Override
     protected void onViewReady(@Nullable Bundle savedInstanceState) {
+        initLineView(line);
+        initLineView(line_two);
+        randomSet(line,line_two);
+    }
+
+    private void initLineView(LineView lineView) {
+        ArrayList<String> test = new ArrayList<String>();
+        Calendar calendar = Calendar.getInstance();
+        for (int i = 0; i < randomint; i++) {
+            calendar.set(Calendar.MONTH, i + 1);
+            String month = String.valueOf(calendar.getTime().getMonth());
+            test.add(month);
+        }
+        lineView.setBottomTextList(test);
+        lineView.setColorArray(new int[]{Color.GREEN,Color.RED});
+        lineView.setDrawDotLine(true);
+        lineView.setShowPopup(LineView.SHOW_POPUPS_NONE);
+    }
+
+    private void randomSet(LineView lineView, LineView lineViewTwo) {
+        ArrayList<Integer> dataList = new ArrayList<>();
+        float random = (float) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataList.add((int) (Math.random() * random));
+        }
+
+        ArrayList<Integer> dataList2 = new ArrayList<>();
+        random = (int) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataList2.add((int) (Math.random() * random));
+        }
+
+        ArrayList<Integer> dataList3 = new ArrayList<>();
+        random = (int) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataList3.add((int) (Math.random() * random));
+        }
+
+        ArrayList<ArrayList<Integer>> dataLists = new ArrayList<>();
+        dataLists.add(dataList);
+
+        lineView.setDataList(dataLists);
+
+        ArrayList<Float> dataListF = new ArrayList<>();
+        float randomF = (float) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataListF.add((float) (Math.random() * randomF));
+        }
+
+        ArrayList<Float> dataListF2 = new ArrayList<>();
+        randomF = (int) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataListF2.add((float) (Math.random() * randomF));
+        }
+
+        ArrayList<Float> dataListF3 = new ArrayList<>();
+        randomF = (int) (Math.random() * 9 + 1);
+        for (int i = 0; i < randomint; i++) {
+            dataListF3.add((float) (Math.random() * randomF));
+        }
+
+        ArrayList<ArrayList<Float>> dataListFs = new ArrayList<>();
+        dataListFs.add(dataListF);
+        dataListFs.add(dataListF2);
+
+        lineViewTwo.setFloatDataList(dataListFs);
     }
 }
